4a7ce516a5bb5bfbfe97ebcbf2b411b53e93ded0
gentcys <iamchencong@gmail.com>
2020-09-25 11:48:19 +0800

    polish home page's view

--- test/integration/page_flow_test.rb
+++ test/integration/page_flow_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class PageFlowTest < ActionDispatch::IntegrationTest
   test "can see home page" do
     get root_path
-    assert_select "h1", "Pages#home"
+    assert_select "h1", "Recipe Generator"
   end
 end
