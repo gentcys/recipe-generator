db23d510dbf0def7be0fd5fb982c048c38e233a5
gentcys <iamchencong@gmail.com>
2020-10-13 14:07:30 +0800

    Add a nav bar

--- test/integration/page_flow_test.rb
+++ test/integration/page_flow_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class PageFlowTest < ActionDispatch::IntegrationTest
-  test "can see home page" do
+  test 'can see home page' do
     get root_path
-    assert_select "h1", "Recipe Generator"
+    assert_select 'h1', 'Recipe Generator'
+  end
+
+  test 'can see nav bar of home page' do
+    get root_path
+    assert_select 'a', 'Recipe Generator'
   end
 end
