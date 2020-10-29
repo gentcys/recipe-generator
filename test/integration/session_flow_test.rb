8c2dbac5677eb4835fd99695e298ec9c32e57cdc
gentcys <iamchencong@gmail.com>
2020-10-29 16:35:20 +0800

    move tests of sign in to session's integration test

---
+++ test/integration/session_flow_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class SessionFlowTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one)
+  end
+
+  test 'sign in with invalid information' do
+    get root_url
+    assert_template 'pages/home'
+    post sessions_create_path, params: { session: { username: '', password: '' } }
+    assert_template layout: false
+    assert_response :bad_request
+    assert 'Failed to sign in! Username or password is incorrect.', response.parsed_body[1]
+  end
+
+  test 'sign in with valid infomation' do
+    get root_url
+    post sessions_create_path, params: { session: { username: @user.username, password: 'foobar' } }
+    assert_template layout: false
+    assert_response :success
+    assert @user.id, session[:user_id]
+  end
+end
--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -9,22 +9,4 @@ class UserFlowTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'pages/home'
   end
-
-  test 'can see nav bar of home page' do
-    get root_path
-    assert_template 'application/_header'
-  end
-
-  test 'sign in with invalid information' do
-    post sessions_create_path, params: { session: { username: '', password: '' } }
-    response_json = JSON.parse(@response.body)
-    assert 'Failed to sign in! Username or password is incorrect.', response_json[1]
-  end
-
-  test 'redirect to user page while sign in successfully' do
-    post sessions_create_path, params: { session: { username: 'MyString', password: 'foobar' } }
-    response_json = JSON.parse(@response.body)
-    assert @user.id, response_json[:id]
-    assert @user.id, session[:user_id]
-  end
 end
